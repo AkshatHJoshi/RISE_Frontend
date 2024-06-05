0dc9737eb9e0428a8e66c1ae15da85dc4e018c49
AkshatHJoshi <joshiakshat27@gmail.com>
2024-06-05 15:28:46 +0530

    Final Logics

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -293,7 +293,7 @@ export class HomeComponent implements OnInit {
     console.log('isbooked', this.todays_booking);
     const currentHour = currentDate.getHours();
 
-    if (isToday && (   (currentHour >= 12 && currentHour < 14) || (currentHour >= 20 && currentHour < 22) )  && this.todays_booking) {
+    if (isToday && (   (currentHour >= 9 && currentHour < 16) || (currentHour >= 20 && currentHour < 22) )  && this.todays_booking) {
       console.log("currentttttttttHour", isToday);
       this.showQrCodeButton = true;
     } else {
